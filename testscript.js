fd6b61ee75f5220ed3f46bed89b5cf3c514d0c77
Semen <semendubynin@gmail.com>
2020-04-13 17:48:22 +0300

    Project3

--- testscript.js
+++ testscript.js
@@ -1,7 +1,18 @@
 'use strict';
 
-let money = +prompt("Ваш бюджет на месяц?", ''),
-	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+let money,time;
+
+function start(){
+	 money = +prompt("Ваш бюджет на месяц?", '');
+	 time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+	 while(isNaN(money)|| money == "" || money == null) {
+		money = +prompt("Ваш бюджет на месяц?", '');
+
+	 }
+}
+start();
+
 
 let appData = {
 	budget: money,
@@ -9,65 +20,77 @@ let appData = {
 	optionalExpenses: {},
 	income: [],
 	timeData: time,
-	savings: false
+	savings: true
 };
 
 
-for (let i = 0; i < 2; i++){
-	let a = prompt("Введите обязатенльную статью расходовв этом месяце", ''),
-		b = +prompt("Во сколько обойдется?", '');
-
-
-	if ((typeof(a))=== "string" && (typeof(a)) != null && (typeof(b))!= null && a != "" && b != "" && a.length < 50) {	
-		console.log("done");
-	appData.expenses[a] = b;
-	} else {
-		console.log("Попробуйте еще раз!");
-		i--;
+function chooseExpenses() {
+	for (let i = 0; i < 2; i++){
+		let a = prompt("Введите обязатенльную статью расходовв этом месяце", ''),
+			b = +prompt("Во сколько обойдется?", '');
+	
+	
+		if ((typeof(a))=== "string" && (typeof(a)) != null && (typeof(b))!= null && a != "" && b != "" && a.length < 50) {	
+			console.log("done");
+		appData.expenses[a] = b;
+		} else {
+			console.log("Попробуйте еще раз!");
+			i--;
+		}
+	
 	}
+}
+chooseExpenses();
+
+function detectDayBudget(){
+	appData.moneyPerDay = (appData.budget / 30).toFixed();
+	alert("Ваш бюджет на сутки " + appData.moneyPerDay );
 
 }
-
-// let i = 0;
-// do {
-//   let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
-//       b = prompt('Во сколько обойдется?', '');
-//       i++;
- 
-//     if((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
-//     console.log('done');
-//     appData.expenses[a] = b;
-//   } else {
-//     console.log('error');
-//   }
-// } while (i < 2);
-
-// while (i < 2) {
-//     let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
-//      b = prompt('Во сколько обойдется?', '');
- 
-//     if((typeof(a)) === 'string' && (typeof(a)) != null && 
-// (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
-//      console.log('done');
-//      appData.expenses[a] = b;
-//     } else {
-//      console.log('error');
-//     }
- 
-//      i++;
-// };
-
-appData.moneyPerDay = appData.budget / 30;
+detectDayBudget();
 
 
-alert("Бюджет из расчета на сутки" + " " + appData.moneyPerDay);
-
-if(appData.moneyPerDay < 100){
+function detectLevel(){
+	if(appData.moneyPerDay < 100){
 console.log("Минимальный уровень достатка");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+	} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
 	console.log("Средний уровень достатка");
-} else  if(appData.moneyPerDay > 2000) {
+}	 else  if(appData.moneyPerDay > 2000) {
 	console.log("Высокий уровень достатка");
-} else {
+	} else {
 	console.log("Попробуйте еще раз,возможно вы где то ошиблись");
 }
+}
+detectLevel();
+
+function checkSaving(){
+	if (appData.savings == true){
+		let save = +prompt("Какова сумма накоплений?", ""),
+			percent = + prompt("Под какой процент?", "");
+
+		appData.monthIncome = save/100/12*percent;
+		alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
+	}
+}
+checkSaving();
+
+function chooseOptExpenses(){
+	for (let i = 0; i < 3; i++){
+		 let questOptExpenses = prompt("Статья необязательных расходов", ""),
+			 summOptExpenses = +prompt ("Сумма необязательных расходов", "");
+		if	((typeof(questOptExpenses)) != null && (typeof(summOptExpenses))!= null && questOptExpenses != "" && summOptExpenses != ""){
+			alert("Старайтесь сократить необязательные расходы!");
+
+		appData.optionalExpenses[questOptExpenses] = summOptExpenses;
+		}else {
+		
+			alert("Отлично, так держать");
+			break;
+
+
+		}
+	}
+}
+chooseOptExpenses();
+
+
